07449658b01df9183bbae8f88adbf217ab506977
KatherineVito <katyjulio16@gmail.com>
2026-03-23 14:49:29 -0500

    Add movie management methods

--- PhpMovies.php
+++ PhpMovies.php
@@ -2,8 +2,26 @@
 
 class PhpMovies
 {
+    private array $movies = [];
+
     public function helloWorld(): string
     {
         return 'Hola, Peliculas';
+    }
+
+    public function addMovie(string $title): void
+    {
+        $title = trim($title);
+
+        if ($title === '') {
+            throw new InvalidArgumentException('El titulo de la pelicula no puede estar vacio.');
+        }
+
+        $this->movies[] = $title;
+    }
+
+    public function getMovies(): array
+    {
+        return $this->movies;
     }
 }
--- index.php
+++ index.php
@@ -3,5 +3,7 @@
 require_once __DIR__ . '/PhpMovies.php';
 
 $phpMovies = new PhpMovies();
+$phpMovies->addMovie('Matrix');
 
 echo $phpMovies->helloWorld() . PHP_EOL;
+echo 'Peliculas registradas: ' . implode(', ', $phpMovies->getMovies()) . PHP_EOL;
